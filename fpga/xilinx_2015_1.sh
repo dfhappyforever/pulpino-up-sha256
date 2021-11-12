45c3f6ce864a0cbcbbce24834905d125108d823c
Dingfu <dfhappyforever@msn.com>
2021-11-12 16:31:12 +0800

    xilinx_2020_2.sh

--- fpga/xilinx_2015_1.sh
+++ fpga/xilinx_2015_1.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#export XILINX_HOME=/home/wanjun/vivado/2015.1
+#you need to set the PATH for vivado:export XILINX_HOME="your vivado install location", example is as follows:
+#export XILINX_HOME=/home/SystemUser/vivado/2015.1
 if [ ! -n "$XILINX_HOME" ]; then
 	echo [ERROR]:XILINX_HOME does not exist, please set. etc:export XILINX_HOME="your vivado install location"
 	exit 1
