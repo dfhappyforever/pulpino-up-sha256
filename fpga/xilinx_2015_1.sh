1de8b9b384f1aa1854c1c59407a75deb8debebb0
Dingfu <dfhappyforever@msn.com>
2021-11-12 17:04:01 +0800

    fix .sh

--- fpga/xilinx_2015_1.sh
+++ fpga/xilinx_2015_1.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#you need to set the PATH for vivado:export XILINX_HOME="your vivado install location", example is as follows:
-#export XILINX_HOME=/home/SystemUser/vivado/2015.1
+# XILINX_HOME must be set for vivado:
+# Assume Vivado 2015.1 is installed at /home/USERNAME/vivado/2015.1
+# For bash:
+#   export XILINX_HOME=/home/USERNAME/vivado/2015.1
 if [ ! -n "$XILINX_HOME" ]; then
-	echo [ERROR]:XILINX_HOME does not exist, please set. etc:export XILINX_HOME="your vivado install location"
-	exit 1
+    echo [ERROR]: XILINX_HOME not set. 
+    echo For bash: export XILINX_HOME="your vivado install location"
+    echo Assume you are using bash, and Vivado 2015.1 is installed at /home/USERNAME/vivado/2015.1
+    echo You have to run this command before sourcing "${BASH_SOURCE[0]}"
+    echo   export XILINX_HOME=/home/USERNAME/vivado/2015.1
 else
-        echo Macro setup pass
+    echo Macro setup pass
+    export VIVADO_HOME=$XILINX_HOME/Vivado/2015.1
+    export VITIS_HOME=$XILINX_HOME/SDK/2015.1
+    export XILINX_VIVADO=$VIVADO_HOME
+    export PATH=$VIVADO_HOME/bin:$VITIS_HOME/bin:$VITIS_HOME/gnu/microblaze/lin/bin:$VITIS_HOME/gnu/arm/lin/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_be/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_le/bin:$XILINX_HOME/DocNav:$PATH
 fi
-export VIVADO_HOME=$XILINX_HOME/Vivado/2015.1
-export VITIS_HOME=$XILINX_HOME/SDK/2015.1
-export XILINX_VIVADO=$VIVADO_HOME
-export PATH=$VIVADO_HOME/bin:$VITIS_HOME/bin:$VITIS_HOME/gnu/microblaze/lin/bin:$VITIS_HOME/gnu/arm/lin/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_be/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_le/bin:$XILINX_HOME/DocNav:$PATH
--- fpga/xilinx_2020_2.sh
+++ fpga/xilinx_2020_2.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
-#you need to set the PATH for vivado:export XILINX_HOME="your vivado install location", example is as follows:
-#export XILINX_HOME=/home/SystemUser/vivado/2020.2
+# XILINX_HOME must be set for vivado:
+# Assume Vivado 2020.2 is installed at /home/USERNAME/vivado/2020.2
+# For bash:
+#   export XILINX_HOME=/home/USERNAME/vivado/2020.2
 if [ ! -n "$XILINX_HOME" ]; then
-	echo [ERROR]:XILINX_HOME does not exist, please set. etc:export XILINX_HOME="your vivado install location"
-	exit 1
+    echo [ERROR]: XILINX_HOME not set. 
+    echo For bash: export XILINX_HOME="your vivado install location"
+    echo Assume you are using bash, and Vivado 2020.2 is installed at /home/USERNAME/vivado/2020.2
+    echo You have to run this command before sourcing "${BASH_SOURCE[0]}"
+    echo   export XILINX_HOME=/home/USERNAME/vivado/2020.2
 else
-        echo Macro setup pass
+    echo Macro setup pass
+    export VIVADO_HOME=$XILINX_HOME/Vivado/2020.2
+    export VITIS_HOME=$XILINX_HOME/Vitis_HLS/2020.2
+    export XILINX_VIVADO=$VIVADO_HOME
+    export PATH=$VIVADO_HOME/bin:$VITIS_HOME/bin:$VITIS_HOME/gnu/microblaze/lin/bin:$VITIS_HOME/gnu/arm/lin/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_be/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_le/bin:$XILINX_HOME/DocNav:$PATH
 fi
-export VIVADO_HOME=$XILINX_HOME/Vivado/2020.2
-export VITIS_HOME=$XILINX_HOME/Vitis_HLS/2020.2
-export XILINX_VIVADO=$VIVADO_HOME
-export PATH=$VIVADO_HOME/bin:$VITIS_HOME/bin:$VITIS_HOME/gnu/microblaze/lin/bin:$VITIS_HOME/gnu/arm/lin/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_be/bin:$VITIS_HOME/gnu/microblaze/linux_toolchain/lin64_le/bin:$XILINX_HOME/DocNav:$PATH
